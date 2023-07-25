0047f7d91ddc0656066aecfe367e50280c3a011e
https-evellybianca <127303756+https-evellybianca@users.noreply.github.com>
2023-07-25 11:49:17 -0300

    Add files via upload

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,13 @@
+console.log("Meu Primeiro Programa: trabalhando com variaveis");
+
+const idade = 26;
+
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomaeda = idade+2;
+console.log(idadeSomaeda);
