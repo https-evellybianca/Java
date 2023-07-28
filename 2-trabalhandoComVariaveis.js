8d3311c57d8ab52b055934737cd299d182484138
https-evellybianca <127303756+https-evellybianca@users.noreply.github.com>
2023-07-28 09:10:50 -0300

    Add files via upload

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,10 @@
+//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+//Js é case sencitive
+
+const idade = 29;
+const nome = "Ricardo";
+let ano = 2020; 
+
+//não fazer
+//mes = "janeiro";
