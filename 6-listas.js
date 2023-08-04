781dac767a6fa6fe5e345cbb99e45a6f77e98530
https-evellybianca <127303756+https-evellybianca@users.noreply.github.com>
2023-08-04 09:02:44 -0300

    Add files via upload

---
+++ 6-listas.js
@@ -0,0 +1,12 @@
+console.log(`Trabalhando com listas`);
+//const salvador = `Salvador`;
+//const saoPaulo = `São Paulo`;
+//const rioDeJaneiro = `Rio De Janeiro`;
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio De Janeiro`
+);
+console.log("Destins possíveis:");
+console.log(salvador, saoPaulo, rioDeJaneiro);
